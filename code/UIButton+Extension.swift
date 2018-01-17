80684cb383e069c12151966b906cb93d9d8e3065
Jeff Kempster <jeffkempster@gmail.com>
2018-01-17 16:47:59 -0500

    Update Extension

--- code/UIButton+Extension.swift
+++ code/UIButton+Extension.swift
@@ -6,21 +6,11 @@
 //  Copyright © 2018 Jeffrey P. Kempster. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
 // Make sure clips to bounds is set in Storboard
 
 @IBDesignable extension UIButton {
-    
-    @IBInspectable var borderWidth: CGFloat {
-        set {
-            layer.borderWidth = newValue
-        }
-        get {
-            return layer.borderWidth
-        }
-    }
     
     @IBInspectable var cornerRadius: CGFloat {
         set {
@@ -28,17 +18,6 @@ import UIKit
         }
         get {
             return layer.cornerRadius
-        }
-    }
-    
-    @IBInspectable var borderColor: UIColor? {
-        set {
-            guard let uiColor = newValue else { return }
-            layer.borderColor = uiColor.cgColor
-        }
-        get {
-            guard let color = layer.borderColor else { return nil }
-            return UIColor(cgColor: color)
         }
     }
     
